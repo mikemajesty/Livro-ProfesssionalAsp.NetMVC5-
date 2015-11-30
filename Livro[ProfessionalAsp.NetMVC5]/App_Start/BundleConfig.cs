fb8545998f4f0f3a362095d6bf9d78a4564d8d24
mikemajesty <mikemajesty2011@gmail.com>
2015-11-30 21:14:02 -0200

    Capítulo 11 - Web API

--- Livro[ProfessionalAsp.NetMVC5]/App_Start/BundleConfig.cs
+++ Livro[ProfessionalAsp.NetMVC5]/App_Start/BundleConfig.cs
@@ -6,7 +6,7 @@ namespace Livro_ProfessionalAsp.NetMVC5_
     public class BundleConfig
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
+        public static void RegisterBundles(BundleCollection bundles) 
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
