f2921061872d1eee2f1d68bb9a26014eebe96f96
mikemajesty <mikemajesty2011@gmail.com>
2015-12-16 19:37:05 -0200

    Capítulo 15 - Extending MVC

---
+++ Livro[ProfessionalAsp.NetMVC5]/Models/IModelBider.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.ModelBinding;
+
+namespace Livro_ProfessionalAsp.NetMVC5_.Models
+{
+
+    public class PointModelBinder : System.Web.ModelBinding.IModelBinder
+    {
+        public bool BindModel(ModelBindingExecutionContext modelBindingExecutionContext, ModelBindingContext bindingContext)
+        {
+            var valueProvider = bindingContext.ValueProvider;
+            int x = (int)valueProvider.GetValue("X").ConvertTo(typeof(int));
+            int y = (int)valueProvider.GetValue("Y").ConvertTo(typeof(int));
+            if (!bindingContext.FallbackToEmptyPrefix)
+                return false;
+            bindingContext = new ModelBindingContext
+            {
+                ModelMetadata = bindingContext.ModelMetadata,
+                ModelState = bindingContext.ModelState,
+                PropertyFilter = bindingContext.PropertyFilter,
+                ValueProvider = bindingContext.ValueProvider
+            };
+            return true;
+        }
+
+    }
+    
+}
